b51e5187a042eb173a6f0f2285924ac531d5af12
Baihan Lin <doerlbh@gmail.com>
2023-09-05 11:07:18 -0700

    [DBG] update options

--- utils_stats.py
+++ utils_stats.py
@@ -189,11 +189,13 @@ def get_boots_seq(
     return boots_seq
 
 
-def get_paired_p_values(ms, ns, ss):
+def get_paired_p_values(ms, ns, ss, df = None):
     m1, m2 = ms
     n1, n2 = ns
     s1, s2 = ss
-    s = np.sqrt(((n1 - 1) * s1**2 + (n2 - 1) * s2**2) / (n1 + n2 - 2))
+    if not df:
+        df = (n1 + n2 - 2)
+    s = np.sqrt(((n1 - 1) * s1**2 + (n2 - 1) * s2**2) / df)
     t = (m1 - m2) / s
-    p = stats.t.sf(np.abs(t), (n1 + n2 - 2)) * 2
+    p = stats.t.sf(np.abs(t), df) * 2
     return p
